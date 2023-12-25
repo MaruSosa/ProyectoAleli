8e6b2d872c027fcafbeea7ffe4d4954272bddfd5
mariana <marianass712@gmail.com>
2023-12-25 19:00:01 -0300

    se realizo correcciones en crear producto

---
+++ js/administrador.js
@@ -0,0 +1,135 @@
+import Producto from "./classProducto.js";
+import { resumenValidaciones } from "./validaciones.js";
+
+
+let formularioProductos = document.getElementById("formProducto");
+let modalProducto = new bootstrap.Modal(
+  document.getElementById("modalProducto")
+);
+const btnCrearProducto = document.querySelector("#btnCrearPelicula");
+const id = document.getElementById("id"),
+  nombre = document.getElementById("nombre"),
+  descripcion = document.getElementById("descripcion"),
+  imagen = document.getElementById("imagen"),
+  precio = document.getElementById("precio");
+
+let altaProducto = true; 
+
+let listaProductos = JSON.parse(localStorage.getItem("listaProductos")) || [];
+
+
+if (listaProductos.length > 0) {
+  listaProductos = listaProductos.map(
+    (Producto) =>
+      new Producto(
+        Producto.id,
+        Producto.nombre,
+        Producto.descripcion,
+        Producto.imagen,
+        Producto.precio,
+      )
+  );
+}
+console.log(listaProductos);
+
+cargaInicial();
+
+function cargaInicial() {
+  if (listaProductos.length > 0) {
+    listaProductos.map((Producto, posicion) =>
+      crearFila(Producto, posicion + 1)
+    );
+  }
+}
+
+function crearFila(Producto, fila) {
+  console.log(Producto);
+  let tablaProducto = document.getElementById("tablaProducto");
+  tablaProducto.innerHTML += `<tr>
+  <th scope="row">${fila}</th>
+  <td>${Producto.nombre}</td>
+  <td><span class="my-class text-truncate">${Producto.descripcion}</span></td>
+  <td><span class="my-class text-truncate">${Producto.imagen}</span></td>
+  <td>${Producto.precio}</td>
+  <td>
+    <button class="btn btn-warning" onclick="prepararProducto('${Producto.id}')">
+      <i class="bi bi-pencil-square"></i>
+    </button>
+    <button class="btn btn-danger" onclick="borrarProducto('${Producto.id}')">
+      <i class="bi bi-x-square"></i>
+    </button>
+  </td>
+</tr>`;
+}
+
+formularioProductos.addEventListener("submit", prepararFormularioProducto);
+btnCrearProducto.addEventListener("click", desplegarModalProducto);
+
+function desplegarModalProducto() {
+  limpiarFormulario();
+  altaProducto=true;
+  modalProducto.show();
+}
+function prepararFormularioProducto(e) {
+    e.preventDefault();
+    console.log("en el evento submit");
+    if (altaProducto) {
+      crearProducto();
+    } else {
+    //   editarProducto();
+    }
+  }
+  
+function crearProducto() {
+ 
+    const resumen = resumenValidaciones(
+      nombre.value,
+      descripcion.value,
+      imagen.value,
+      precio.value
+    );
+
+    mostrarMensajeError(resumen);
+    if (resumen.length === 0) {
+      // crear producto
+      const productoNuevo = new Producto(
+        undefined,
+        nombre.value,
+        descripcion.value,
+        imagen.value,
+        precio.value
+      );
+      
+      listaProductos.push(productoNuevo);
+      //guardar el array en localstorage
+      guardarEnLocalstorage();
+      console.log(productoNuevo);
+      //dibujar la fila en la tabla
+      crearFila(productoNuevo, listaProductos.length);
+      //mostrar un mensaje
+      Swal.fire(
+        "producto creado",
+        "El producto fue creado exitosamente",
+        "success"
+      );
+      limpiarFormulario();
+    
+    }
+  }
+  
+  function mostrarMensajeError(resumen) {
+    if (resumen.length > 0) {
+      alert.className = "alert alert-danger mt-3";
+      alert.innerHTML = resumen;
+    } else {
+      alert.className = "alert alert-danger mt-3 d-none";
+    }
+  }
+  
+  function guardarEnLocalstorage() {
+    localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
+  }
+  
+  function limpiarFormulario() {
+    formularioProductos.reset();
+  }
---
+++ js/validaciones.js
@@ -0,0 +1,52 @@
+function validarCantidadCaracteres(texto, min, max){
+    if(texto.length >= min && texto.length <= max){
+        console.log('la palabra es valida')
+        return true;
+    }else{
+        console.log('la palabra es incorrecta')
+        return false;
+    }
+  }
+  function validarCantidadCaracteresDescripcion(texto,min,max){
+      if(texto.length>=min&& texto.length<=max){
+          return true;
+      }else{
+          return false;
+      }
+  }
+  function validarImagen (url){
+    const patron = /\.(jpg|jpeg|png|gif)$/i;
+    if (patron.test(url)) {
+      console.log('La URL de la imagen tiene un formato válido');
+      return true;
+    } else {
+      console.log('La URL de la imagen tiene un formato erróneo');
+      return false;
+    }
+  };
+  function validarPrecio(precio){
+      const regex = /^(?!0+(\.0+)?$)(?!-0+(\.0+)?$)\d+(\.\d+)?$/;
+      return regex.test(precio);
+    }
+  
+  
+  export function resumenValidaciones(nombre, descripcion, imagen, precio){
+    let resumen = '';
+    if(! validarCantidadCaracteres(nombre,2, 100)){
+  
+        resumen = 'El nombre debe contener entre 2 y 100 caracteres <br>';
+    };
+    if(! validarCantidadCaracteresDescripcion(descripcion,5, 300)){
+      
+        resumen += 'La descripcion debe contener entre 5 y 300 caracteres <br>';
+    };
+    if(!validarImagen(imagen)){
+        
+        resumen += 'La url debe ser valida y contener una extension (.jpg,.png o .gif) <br>';
+    };
+    if(! validarPrecio(precio)){
+        
+        resumen += 'Debe ingresar un numero mayor a 0 <br>';
+    };
+    return resumen;
+  }
