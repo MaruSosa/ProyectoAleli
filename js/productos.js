abeb37170e69074f9d235aeeb6ee8cd2b532a4c8
mariana <marianass712@gmail.com>
2023-12-26 16:29:36 -0300

    se realizo la pagina de detalle

---
+++ js/productos.js
@@ -0,0 +1,23 @@
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductos")) || [];
+
+listaProductos.map((producto) => {
+  crearColumna(producto);
+});
+
+function crearColumna(producto) {
+  let grilla = document.querySelector("#grilla");
+  grilla.innerHTML += `
+    <aside class="col-12 col-md-4 col-lg-3 mb-3">
+    <div class="card h-100" >
+      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+      </div>
+      <div class='card-footer'>
+      <a class="btn btn-primary" href='../pages/detalle.html'>ver detalle</a>
+      </div>
+    </div>
+  </aside>
+    `;
+}
