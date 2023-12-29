cf771086e35b5593830660441332e1ec63b12bad
mariana <marianass712@gmail.com>
2023-12-29 19:49:22 -0300

    le cambie el tamaño de las letras del detalle

--- js/detalle.js
+++ js/detalle.js
@@ -1,18 +1,20 @@
-
 console.log(window.location.search);
 const parametroURL = new URLSearchParams(window.location.search);
-const idProducto = parametroURL.get('id');
+const idProducto = parametroURL.get("id");
 
 console.log(idProducto);
 
-const listaNuevaProductos = JSON.parse(localStorage.getItem('listaProductos')) || []; // Manejar el caso en que no haya datos en localStorage
+const listaNuevaProductos =
+  JSON.parse(localStorage.getItem("listaProductos")) || []; // Manejar el caso en que no haya datos en localStorage
 
 if (idProducto && listaNuevaProductos.length > 0) {
-  const productoBuscado = listaNuevaProductos.find(itemProducto => itemProducto.id === idProducto);
+  const productoBuscado = listaNuevaProductos.find(
+    (itemProducto) => itemProducto.id === idProducto
+  );
 
   if (productoBuscado) {
-    const mainDetalleProducto = document.querySelector('main');
-    mainDetalleProducto.innerHTML +=  `<article class="card mb-3">
+    const mainDetalleProducto = document.querySelector("main");
+    mainDetalleProducto.innerHTML += `<article class="card mb-3">
       <div class="row g-0 p-3">
         <div class="col-md-4">
           <img
@@ -23,10 +25,10 @@ if (idProducto && listaNuevaProductos.length > 0) {
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title justify-content-center">${productoBuscado.nombre}</h5>
-            <p class="card-text">${productoBuscado.descripcion}</p>
+            <h5 class="card-title justify-content-center" style="font-size:50px">${productoBuscado.nombre}</h5>
+            <p class="card-text" style="font-size:30px">${productoBuscado.descripcion}</p>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">Precio: ${productoBuscado.precio}</li>
+              <li class="list-group-item" style="font-size:30px">Precio: ${productoBuscado.precio}</li>
             </ul>
           </div>
         </div>
@@ -34,8 +36,12 @@ if (idProducto && listaNuevaProductos.length > 0) {
     </article>
   `;
   } else {
-    console.log('El producto con el ID proporcionado no se encuentra en la lista.');
+    console.log(
+      "El producto con el ID proporcionado no se encuentra en la lista."
+    );
   }
 } else {
-  console.log('No se proporcionó un ID en la URL o no hay productos en la lista.');
+  console.log(
+    "No se proporcionó un ID en la URL o no hay productos en la lista."
+  );
 }
